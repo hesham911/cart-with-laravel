1454b07efb60302bdd225d640ab72aae631612ef
hesham.hanafi911@gmail.com <hesham.hanafi911@gmail.com>
2021-05-20 03:03:54 +0200

    add logout link

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -40,29 +40,17 @@
                     <button type="button" class="overlay-close1">
                         <i class="fa fa-times" aria-hidden="true"></i>
                     </button>
-                    <div class="wrap">
-                        <h5 class="text-center mb-4">Login Now</h5>
-                        <div class="login p-5 bg-dark mx-auto mw-100">
-                            <form action="#" method="post">
-                                <div class="form-group">
-                                    <label class="mb-2">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" required="">
-                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                                </div>
-                                <div class="form-group">
-                                    <label class="mb-2">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="" required="">
-                                </div>
-                                <div class="form-check mb-2">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                </div>
-                                <button type="submit" class="btn btn-primary submit mb-4">Sign In</button>
-
-                            </form>
-                        </div>
-                        <!---->
+                    <div>
+                        <a class="btn btn-dark m-5" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </div>
+
                 </div>
                 <!---->
             </div>
